093d8e9714b6057b86694f30baa8ef1eab322baa
gaet kosi <GAETANKOSI1611@GMAIL.COM>
2025-07-31 20:19:26 -0400

    TEST LIMITE

--- src/main/java/tp/gestion/GestionReservation.java
+++ src/main/java/tp/gestion/GestionReservation.java
@@ -62,33 +62,36 @@ public class GestionReservation
             if (chambre == null)
                 throw new AubergeInnException("Chambre inexistante: " + idChambre);
 
+            // Vérifie des contraintes logiques de la période
+            validerReservationLogique(date_debut, date_fin);
+
             // Vérifier que la chambre n'est pas déjà réservée dans la période donnée
             List<Reservation> listReservationChambre = reservations.getReservationChambre(idChambre);
 
-            if (listReservationChambre.size() > 0) {
+           // if (listReservationChambre.size() > 0) {
                 for (Reservation res : listReservationChambre) {
 
                     if (date_debut.before(res.getDate_fin()) && res.getDate_debut().before(date_fin))
                         throw new AubergeInnException("la Chambre " + idChambre + "a déjà réservée pour cette période.");
                 }
-            }
+            //}
 
 
             // Vérifier que le client n'a pas déjà une réservation dans la période donnée
             List<Reservation> listReservationClient = reservations.getReservationClient(idClient);
 
-            if (listReservationClient.size() > 0) {
+            //if (listReservationClient.size() > 0) {
                 for (Reservation res : listReservationClient) {
                     if (date_debut.before(res.getDate_fin()) && res.getDate_debut().before(date_fin))
 
                         throw new AubergeInnException("Le client " + idClient + " a déjà une réservation pour cette période.");
                 }
-            }
+            //}
 
 
             // Vérifier que la date de réservation n'est pas dans le passé
-            if (date_debut.before(new Date()))
-                throw new AubergeInnException("La date de réservation ne peut pas être dans le passé.");
+            //if (date_debut.before(new Date()))
+            //    throw new AubergeInnException("La date de réservation ne peut pas être dans le passé.");
 
 
             double prix_total = chambre.getPrixTotal();
@@ -103,5 +106,66 @@ public class GestionReservation
         }
     }
 
-}
+    // Afficher les chambres libres entre deux dates
+    public void afficherChambresLibres(Date date_debut, Date date_fin) throws Exception {
+        validerPeriodeAffichage(date_debut, date_fin);
+
+        List<Chambre> toutesLesChambres = chambres.getAllChambres();
+        boolean chambreTrouvee = false;
+
+        System.out.println("Chambres libres entre " + date_debut + " et " + date_fin + " :");
+
+        for (Chambre chambre : toutesLesChambres) {
+            boolean estLibre = true;
+
+            List<Reservation> reservationsDeLaChambre = reservations.getReservationChambre(chambre.getIdChambre());
+            for (Reservation res : reservationsDeLaChambre) {
+                if (date_debut.before(res.getDate_fin()) && res.getDate_debut().before(date_fin)) {
+                    estLibre = false;
+                    break;
+                }
+            }
+
+            if (estLibre) {
+                chambreTrouvee = true;
+                System.out.println("- Chambre " + chambre.getIdChambre() + " : " + chambre.getNom_chambre());
+            }
+        }
+
+        if (!chambreTrouvee) {
+            System.out.println("Aucune chambre libre pour cette période.");
+        }
+    }
+
+    // Méthode privée pour valider une réservation
+    private void validerReservationLogique(Date date_debut, Date date_fin) throws AubergeInnException {
+        Date maintenant = new Date();
+
+        if (date_debut.before(maintenant)) {
+            throw new AubergeInnException("La date de réservation ne peut pas être dans le passé.");
+        }
+
+        if (!date_debut.before(date_fin)) {
+            throw new AubergeInnException("La date de début doit être strictement inférieure à la date de fin.");
+        }
+
+        long dureeMillis = date_fin.getTime() - date_debut.getTime();
+        long dureeJours = dureeMillis / (1000 * 60 * 60 * 24);
+        if (dureeJours < 1) {
+            throw new AubergeInnException("Une réservation doit durer au moins une journée complète.");
+        }
+    }
+
+    // Méthode privée pour valider une période d’affichage
+    private void validerPeriodeAffichage(Date date_debut, Date date_fin) throws AubergeInnException {
+        if (!date_debut.before(date_fin)) {
+            throw new AubergeInnException("La date de début doit être strictement inférieure à la date de fin.");
+        }
+
+        long dureeJours = (date_fin.getTime() - date_debut.getTime()) / (1000 * 60 * 60 * 24);
+        if (dureeJours < 1) {
+            throw new AubergeInnException("La période d'affichage doit couvrir au moins une journée.");
+        }
+    }
+    }
 
